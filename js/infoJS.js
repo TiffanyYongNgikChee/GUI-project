80bbffd254652b1d496eeffa4bb9760ef52258ad
wilsonsong1 <G00418330@atu.ie>
2023-04-28 17:23:14 +0100

    Fixed infoJS

--- js/infoJS.js
+++ js/infoJS.js
@@ -4,47 +4,59 @@ sessionStorage.ticketLimit2 = 4;
 sessionStorage.ticketLimit3 = 4;
 
 //Uncomment lines below to change number of tickets available
-//localStorage.totalTickets1 = 50;
-//localStorage.totalTickets2 = 50;
-//localStorage.totalTickets3 = 50;
+localStorage.totalTickets1 = 50;
+localStorage.totalTickets2 = 50;
+localStorage.totalTickets3 = 50;
 //=================================================================================================================================================================================
 //Add tickets to cart (Into Session Storage) Limits a person to be only able to purchase 4 tickets in one session.
+var a1 = 0;
+var b1 = 0;
+var a2 = 0;
+var b2 = 0;
+var a3 = 0;
+var b3 = 0;
+
 function add(qty){
 
 //First Event----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    if(qty == 1){
-        t = localStorage.totalTickets1;
+if(qty == 1){
+    t = localStorage.totalTickets1;
 
-        num = document.getElementById("ticket1").value;
+    num = document.getElementById("ticket1").value;
 
-        if(num <= 4){
-            y = sessionStorage.ticketLimit1;
+    if(num <= 4){
+        y = sessionStorage.ticketLimit1;
 
-            if(num <= y){
+        if(num <= y){
 
-                if(num <= t){
-                    
-                    sessionStorage.qty1 = document.getElementById("ticket1").value;
-                    x = localStorage.totalTickets1;
-        
-                    localStorage.totalTickets1 = x - sessionStorage.qty1;
-                    sessionStorage.ticketLimit1 = y - sessionStorage.qty1;
-                }
-                else{
-                    alert("Sorry, there are not enough tickets available at the moment. There are " + (x - num) + " tickets left.");
-                }
+            if(num <= t){
+
+                a1 = document.getElementById("ticket1").value;
+
+                x = localStorage.totalTickets1;
+    
+                localStorage.totalTickets1 = x - a1;
+                sessionStorage.ticketLimit1 = y - a1;
+
+                b1 += a1 * 1;
+
+                sessionStorage.qty1 = b1;
+
             }
             else{
-                alert("Sorry, the maximum amount of tickets to purchase is 4");
+                alert("Sorry, there are not enough tickets available at the moment. There are " + (x - num) + " tickets left.");
             }
         }
-
         else{
-
             alert("Sorry, the maximum amount of tickets to purchase is 4");
         }
     }
 
+    else{
+
+        alert("Sorry, the maximum amount of tickets to purchase is 4");
+    }
+}
 //Second Event---------------------------------------------------------------------------------------------------------------------------------------------------------------------
     if(qty == 2){
         t = localStorage.totalTickets2;
@@ -58,11 +70,16 @@ function add(qty){
 
                 if(num <= t){
                     
-                    sessionStorage.qty2 = document.getElementById("ticket2").value;
+                    a2 = document.getElementById("ticket2").value;
+
                     x = localStorage.totalTickets2;
         
-                    localStorage.totalTickets2 = x - sessionStorage.qty2;
-                    sessionStorage.ticketLimit2 = y - sessionStorage.qty2;
+                    localStorage.totalTickets2 = x - a2;
+                    sessionStorage.ticketLimit2 = y - a2;
+    
+                    b2 += a2 * 1;
+    
+                    sessionStorage.qty2 = b2;
                 }
                 else{
                     alert("Sorry, there are not enough tickets available at the moment. There are " + (x - num) + " tickets left.");
@@ -92,11 +109,16 @@ function add(qty){
 
                 if(num <= t){
                     
-                    sessionStorage.qty3 = document.getElementById("ticket3").value;
+                    a3 = document.getElementById("ticket3").value;
+
                     x = localStorage.totalTickets3;
         
-                    localStorage.totalTickets3 = x - sessionStorage.qty3;
-                    sessionStorage.ticketLimit3 = y - sessionStorage.qty3;
+                    localStorage.totalTickets3 = x - a3;
+                    sessionStorage.ticketLimit3 = y - a3;
+    
+                    b3 += a3 * 1;
+    
+                    sessionStorage.qty3 = b3;
                 }
                 else{
                     alert("Sorry, there are not enough tickets available at the moment. There are " + (x - num) + " tickets left.");
